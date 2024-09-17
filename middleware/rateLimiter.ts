c506ddbd3db107cea2c16f9d2d577d58ae0ebde5
Deepak <deepakkumar@bitqit.com>
2024-09-17 10:41:30 +0530

    udpate

--- middleware/rateLimiter.ts
+++ middleware/rateLimiter.ts
@@ -6,6 +6,7 @@ export const rateLimiter = rateLimit({
   db: new Redis(),
   duration: 60 * 1000,
   errorMessage: 'Too many requests from this IP, please try again after some time.',
+
   id: (ctx: any) => ctx.ip, // Using the IP as the unique identifier
   headers: {
     remaining: 'Rate-Limit-Remaining',
